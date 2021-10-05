79076ecf952dee6d08bfe722d2b20f87f9e7be0f
sanathkumar06 <42084450+sanathkumar06@users.noreply.github.com>
2021-10-06 00:16:53 +0530

    Update Sort012.java

--- Sort012.java
+++ Sort012.java
@@ -1,1 +1,32 @@
 
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[] = {0,2,2,1,0,0,2,1};
+		
+		int a, i = 0, j=0, k=0;
+		for(a=0; a< arr.length; a++)
+		{
+		    if(arr[a]==0) i++;
+		    else if(arr[a] == 1) j++;
+		    else k++;
+		}
+		for(a = 0; a < arr.length; a++){
+	        if(i > 0) {
+	            i--;
+	            arr[a] = 0;
+	        }
+	        else if(j > 0) {
+	            j--;
+	            arr[a] = 1;
+	        }
+	        else if(k > 0) {
+	            k--;
+	            arr[a] = 2;
+	        }
+		}
+		for(a=0; a < arr.length; a++){
+		    System.out.println(arr[a]);
+		}
+	}
+}
